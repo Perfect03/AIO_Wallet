88f39c01f004006ce82b14a7fcc4d864643743e0
ThomasAqu1nas <andranikoganyan27@gmail.com>
2023-05-04 21:18:43 +0700

    feat: implementation of the server part

--- src/scripts/rpc/defaultProvider.ts
+++ src/scripts/rpc/defaultProvider.ts
@@ -2,6 +2,6 @@ import { ethers } from 'ethers';
 
 const url = process.env.REACT_APP_RPC_ENDPOINT;
 
-const defaultProvider = new ethers.providers.JsonRpcProvider(url, 56);
+const defaultProvider = new ethers.providers.JsonRpcProvider(url);
 
 export default defaultProvider;

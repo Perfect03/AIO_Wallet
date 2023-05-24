17bfde3ec0dca425ccccaadf1d47036e261ebbf2
4ndrei <andreygames5@mail.ru>
2023-05-24 19:41:37 +0700

    feat: header connect button

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -5,6 +5,7 @@ import { ExternalProvider } from '@ethersproject/providers';
 declare global {
   interface Window {
     ethereum?: ExternalProvider & { selectedAddress: string };
+    request?: (param: { method: string; params: Array }) => void;
   }
 
   interface URLSearchParams {

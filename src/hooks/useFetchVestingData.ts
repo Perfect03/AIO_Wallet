14692b8207eb4b0849a85705f53344a6ee425d2a
4ndrei <andreygames5@mail.ru>
2023-06-23 12:05:42 +0700

    fix: minor

--- src/hooks/useFetchVestingData.ts
+++ src/hooks/useFetchVestingData.ts
@@ -8,24 +8,28 @@ export default function useFetchVestingData(): [number, number] {
 
   useEffect(() => {
     (async () => {
-      const contract = getPresaleContract();
-      const account = (await metamaskProvider?.send('eth_accounts', []))[0];
+      try {
+        const contract = getPresaleContract();
+        const account = (await metamaskProvider?.send('eth_accounts', []))[0];
 
-      const newLockedAmt = await contract['userVesting(address)'](account);
-      setLockedAmt(newLockedAmt[0].sub(newLockedAmt[1]).div(1e9).toNumber());
+        const newLockedAmt = await contract['userVesting(address)'](account);
+        setLockedAmt(newLockedAmt[0].sub(newLockedAmt[1]).div(1e9).toNumber());
 
-      const newClaimableAmt = await contract['claimableAmount(address)'](account);
-      setClaimableAmt(newClaimableAmt.div(1e9).toNumber());
+        const newClaimableAmt = await contract['claimableAmount(address)'](account);
+        setClaimableAmt(newClaimableAmt.div(1e9).toNumber());
 
-      const listener = async (user: string) => {
-        if (user.toLowerCase() == account.toLowerCase()) {
-          const newLockedAmt = await contract['userVesting(address)'](account);
-          setLockedAmt(newLockedAmt[0].div(1e9).toNumber());
-        }
-      };
-      contract.on('Purchase', listener);
+        const listener = async (user: string) => {
+          if (user.toLowerCase() == account.toLowerCase()) {
+            const newLockedAmt = await contract['userVesting(address)'](account);
+            setLockedAmt(newLockedAmt[0].div(1e9).toNumber());
+          }
+        };
+        contract.on('Purchase', listener);
 
-      return () => contract.off('Purchase', listener);
+        return () => contract.off('Purchase', listener);
+      } catch (err) {
+        console.log(err);
+      }
     })();
   }, []);
 
